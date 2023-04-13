53432292ba7b56723cd594aa608c8d52854c4e95
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2023-04-12 20:28:40 -0500

    Revert "Dont need a robot's discovery"
    This reverts commit f7c8f6a70de70c45377ddac57fe3c32870990007.

---
+++ common/src/main/java/io/github/consistencyplus/consistency_plus/core/extensions/CPlusTransparentBlock.java
@@ -0,0 +1,9 @@
+package io.github.consistencyplus.consistency_plus.core.extensions;
+
+import net.minecraft.block.TransparentBlock;
+
+public class CPlusTransparentBlock extends TransparentBlock {
+	public CPlusTransparentBlock(Settings settings) {
+		super(settings);
+	}
+}
