c1e74e7e24881182ba41edb2729a86977d422328
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2023-04-12 20:28:16 -0500

    Revert "Fishes love me, Forges fear me."
    This reverts commit ef1269db309a20f5d99c2e449e892c4b0e541f12.

---
+++ forge/src/main/java/io/github/consistencyplus/consistency_plus/ConsistencyPlus.java
@@ -0,0 +1,10 @@
+package io.github.consistencyplus.consistency_plus;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ConsistencyPlusMain.ID)
+public class ConsistencyPlus {
+	public ConsistencyPlus() {
+		ConsistencyPlusMain.init();
+	}
+}
